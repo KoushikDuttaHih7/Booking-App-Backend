5d05e1bb34f08da1e0ce3c8a221318cdc5ac5600
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-07-03 14:57:42 +0530

    Create First API using Node.js

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ import dotenv from 'dotenv'
 dotenv.config()
 import connectDB from './config/database.js'
 connectDB()
+// Routes
+import authRoutes from './routes/authRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import hotelRoutes from './routes/hotelRoutes.js'
+import roomRoutes from './routes/roomRoutes.js'
+
 const app = express()
 const PORT = process.env.PORT
 
+// Middleware
+app.use('/api/auth',authRoutes)
+app.use('/api/user',userRoutes)
+app.use('/api/hotel',hotelRoutes)
+app.use('/api/room',roomRoutes)
 
 app.listen(PORT,()=>{
     console.log(`Connected to Port ${PORT}`);
