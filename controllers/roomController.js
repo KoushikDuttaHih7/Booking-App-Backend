dc9f6ef2809c17ab4f392ea9fffb411b24800675
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-07-05 12:09:36 +0530

    Node MongoDB Model Relations

---
+++ controllers/roomController.js
@@ -0,0 +1,80 @@
+import roomModel from "../models/roomModel.js";
+import hotelModel from "../models/hotelModel.js";
+import { createError } from "../utils/error.js";
+
+// CREATE
+export const createRoom = async (req, res, next) => {
+  const hotelId = req.params.hotelid;
+  const newRoom = new roomModel(req.body);
+
+  try {
+    const savedRoom = await newRoom.save();
+    try {
+      await hotelModel.findByIdAndUpdate(hotelId, {
+        $push: { rooms: savedRoom.id },
+      });
+    } catch (err) {
+      next(err);
+    }
+    res.status(200).json(savedRoom);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// UPDATE
+export const updateRoom = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const updatedRoom = await roomModel.findByIdAndUpdate(
+      id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedRoom);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// DELETE
+export const deleteRoom = async (req, res, next) => {
+  const hotelId = req.params.hotelid;
+  const { id } = req.params;
+  try {
+    await roomModel.findByIdAndDelete(id);
+    try {
+      await hotelModel.findByIdAndUpdate(hotelId, {
+        $pull: { rooms: id },
+      });
+    } catch (err) {
+      next(err);
+    }
+    res.status(200).json("Room has been deleted");
+  } catch (err) {
+    next(err);
+  }
+};
+
+// GET ONE
+export const getOneRoom = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const room = await roomModel.findById(id);
+    res.status(200).json(room);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// GET ALL
+export const getRooms = async (req, res, next) => {
+  try {
+    const rooms = await roomModel.find();
+    res.status(200).json(rooms);
+  } catch (err) {
+    next(err);
+  }
+};
--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -1,8 +1,23 @@
 import express from "express";
 const router = express.Router();
+import {
+  createRoom,
+  deleteRoom,
+  getOneRoom,
+  getRooms,
+  updateRoom,
+} from "../controllers/roomController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
-router.get('/',(req,res)=>{
-    res.send('Hello, this is auth endpoint')
-})
+// CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
+// UPDATE
+router.put("/:id", verifyAdmin, updateRoom);
+// DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+// GET ONE
+router.get("/:id", getOneRoom);
+// GET ALL
+router.get("/", getRooms);
 
-export default router
+export default router;
