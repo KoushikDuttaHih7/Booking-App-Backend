3c7e4fc94a4ae178ae61684d8bdd2bada16c050b
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-07-18 11:24:45 +0530

    Admin Login

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,14 +25,15 @@ export const register = async (req, res, next) => {
 
 // LOGIN
 export const login = async (req, res, next) => {
-  const { username, password } = req.body;
+  // const { username, password } = req.body;
   try {
-    const user = await userModel.findOne({
-      username: username,
-    });
+    const user = await userModel.findOne({ username: req.body.username });
     if (!user) return next(createError(400, "Invalid username & password"));
 
-    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
     if (!isPasswordCorrect)
       return next(createError(400, "Invalid username & password"));
 
@@ -41,18 +42,21 @@ export const login = async (req, res, next) => {
       process.env.JWT_SECRET
     );
 
+    const { password, isAdmin, ...otherDetails } = user._doc;
     res
       .cookie("accessToken", token, {
         httpOnly: true,
       })
       .status(200)
-      .json({
-        _id: user._id,
-        username: user.username,
-        email: user.email,
-        createdAt: user.createdAt,
-        updatedAt: user.updatedAt,
-      });
+      // .json({
+      //   _id: user._id,
+      //   username: user.username,
+      //   email: user.email,
+      //   createdAt: user.createdAt,
+      //   updatedAt: user.updatedAt,
+      //   isAdmin,
+      // });
+      .json({ details: { ...otherDetails }, isAdmin });
   } catch (err) {
     next(err);
   }
